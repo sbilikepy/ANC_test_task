eae77e47a1b873c05b9322e80a0ac8dcdc7fe804
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-13 09:04:42 +0300

    get_queryset and get_context_data for sort and search EmployeeListView table

--- staff_management/views.py
+++ staff_management/views.py
@@ -74,3 +74,36 @@ class EmployeeListView(LoginRequiredMixin, generic.ListView):
     context_object_name = "employee_list"
     template_name = "staff_management/employee/employee_list.html"
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # sort things
+        sort = self.request.GET.get('sort', 'position__hierarchy_level')
+        direction = self.request.GET.get('direction', 'asc')
+        if direction == 'desc':
+            sort = f'-{sort}'
+
+        # search things
+        search_form = EmployeeSearchForm(self.request.GET)
+        if search_form.is_valid():
+            search_query = search_form.cleaned_data.get('name')
+            if search_query:
+                queryset = queryset.filter(
+
+                    Q(full_name__icontains=search_query) |
+                    Q(first_name__icontains=search_query) |
+                    Q(last_name__icontains=search_query) |
+
+                    Q(email__icontains=search_query) |
+
+                    Q(position__name__icontains=search_query)
+
+                )
+
+        return queryset.order_by(sort)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = EmployeeSearchForm(self.request.GET)
+        return context
