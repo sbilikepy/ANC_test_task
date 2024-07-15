6ced9c0b30856a91c694eb8c53904789bfada7e5
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-15 04:36:34 +0300

    paths for load_employees_for_list_view (Ajax), EmployeeCreateView, EmployeeUpdateView, EmployeeDeleteView, EmployeeDetailView and crud info

--- staff_management/urls.py
+++ staff_management/urls.py
@@ -8,7 +8,32 @@ urlpatterns = [
     path("tree/", EmployeeTreeView.as_view(), name="employee-tree"),
     path("load_subordinates/", load_subordinates, name="load-subordinates"),
     path("employees/", EmployeeListView.as_view(), name="employee-list"),
-    ####### + CRUD HERE ###########
-
+    path(
+        "load_employees/",
+        load_employees_for_list_view,
+        name="load-employees-for-list-view",
+    ),
+    # CRUD URLs
+    path(
+        "employees/create/",
+        EmployeeCreateView.as_view(),
+        name="employee-create"
+    ),
+    path(
+        "employees/<int:pk>/update/",
+        EmployeeUpdateView.as_view(),
+        name="employee-update",
+    ),
+    path(
+        "employees/<int:pk>/delete/",
+        EmployeeDeleteView.as_view(),
+        name="employee-delete",
+    ),
+    path(
+        "employees/<int:pk>/",
+        EmployeeDetailView.as_view(),
+        name="employee-detail"
+    ),
+    path("employees/crud-info/", crud_info, name="employee-crud-info"),
 ]
 
