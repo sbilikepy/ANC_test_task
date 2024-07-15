5000de9b7e121c02b726c2f5ce92d70a29810f18
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-15 04:39:39 +0300

    css

--- staff_management/views.py
+++ staff_management/views.py
@@ -15,28 +15,20 @@ def index(request: HttpRequest) -> HttpResponse:
         template_name="staff_management/index.html",
         context={
             "db_size": Employee.objects.count(),
-
             "lvl_7": Employee.objects.filter(
                 position__hierarchy_level=7).count(),
-
             "lvl_6": Employee.objects.filter(
                 position__hierarchy_level=6).count(),
-
             "lvl_5": Employee.objects.filter(
                 position__hierarchy_level=5).count(),
-
             "lvl_4": Employee.objects.filter(
                 position__hierarchy_level=4).count(),
-
             "lvl_3": Employee.objects.filter(
                 position__hierarchy_level=3).count(),
-
             "lvl_2": Employee.objects.filter(
                 position__hierarchy_level=2).count(),
-
             "lvl_1": Employee.objects.filter(
                 position__hierarchy_level=1).count(),
-
         },
     )
 
