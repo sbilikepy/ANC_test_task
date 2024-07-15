4a68bbeb24241e55e6b46050b136a6be265da589
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-15 04:27:08 +0300

    imports upd

--- staff_management/models.py
+++ staff_management/models.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Count
+from django.urls import reverse
 
 
 class Employee(AbstractUser):
