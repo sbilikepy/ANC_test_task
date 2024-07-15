443948bdf4549e31bfe2789f36acda73c4934c64
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-07-15 04:35:16 +0300

    flake

--- EmployeeManagementDjangoProject/urls.py
+++ EmployeeManagementDjangoProject/urls.py
@@ -3,11 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "", include(
-            "staff_management.urls",
-            namespace="staff_management"
-        )
-    ),
+    path("", include("staff_management.urls", namespace="staff_management")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
